14877875c6df03631e7f094d4d622655c1ee06f7
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-06-09 16:34:51 +0530

    Update Globe.tsx

--- src/components/globe/Globe.tsx
+++ src/components/globe/Globe.tsx
@@ -1,42 +1,63 @@
 "use client";
 
-import { Canvas } from "@react-three/fiber";
-import {
-  OrbitControls,
-  Stars,
-} from "@react-three/drei";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
 
-function Earth() {
-  return (
-    <mesh rotation={[0.3, 1, 0]}>
-      <sphereGeometry args={[2, 64, 64]} />
-      <meshStandardMaterial
-        color="#1a3d7c"
-        emissive="#4FD1FF"
-        emissiveIntensity={0.3}
-      />
-    </mesh>
+const Globe = dynamic(
+  () => import("react-globe.gl"),
+  {
+    ssr: false,
+  }
+);
+
+const heritageData = [
+  {
+    lat: 43.06,
+    lng: 141.35,
+    size: 0.4,
+    color: "#ff4d4d",
+    label: "Ainu Language (Japan) • Risk 91%",
+  },
+
+  {
+    lat: 11.4,
+    lng: 76.7,
+    size: 0.35,
+    color: "#FFD166",
+    label: "Toda Embroidery (India) • Risk 82%",
+  },
+
+  {
+    lat: 13.4,
+    lng: 122.5,
+    size: 0.4,
+    color: "#ff4d4d",
+    label: "Fishing Songs (Philippines) • Risk 88%",
+  },
+];
+
+export default function GlobeView() {
+  const points = useMemo(
+    () => heritageData,
+    []
   );
-}
 
-export default function Globe() {
   return (
-    <div className="h-[700px] w-full">
-      <Canvas camera={{ position: [0, 0, 6] }}>
-        <ambientLight intensity={1.5} />
-
-        <pointLight
-          position={[10, 10, 10]}
-        />
-
-        <Stars />
-
-        <Earth />
-
-        <OrbitControls
-          enableZoom={true}
-        />
-      </Canvas>
+    <div className="w-full h-[800px]">
+      <Globe
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+        bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+        backgroundColor="#060B17"
+        pointsData={points}
+        pointLat="lat"
+        pointLng="lng"
+        pointAltitude="size"
+        pointColor="color"
+        pointRadius={0.25}
+        pointLabel="label"
+        atmosphereColor="#4FD1FF"
+        atmosphereAltitude={0.15}
+      />
     </div>
   );
 }

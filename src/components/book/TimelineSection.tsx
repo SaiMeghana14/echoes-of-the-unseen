447d4d6d82bd61a9e4937642d816dbba7a8072ad
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-06-10 12:13:16 +0530

    Create TimelineSection.tsx

---
+++ src/components/book/TimelineSection.tsx
@@ -0,0 +1,34 @@
+interface TimelineSectionProps {
+  timeline: string[];
+}
+
+export default function TimelineSection({
+  timeline,
+}: TimelineSectionProps) {
+  return (
+    <div className="glass rounded-3xl p-8">
+
+      <h2 className="text-3xl font-bold mb-8">
+        🕰 Heritage Timeline
+      </h2>
+
+      <div className="space-y-6">
+
+        {timeline.map((item) => (
+          <div
+            key={item}
+            className="
+            p-4
+            rounded-xl
+            bg-white/5
+          "
+          >
+            {item}
+          </div>
+        ))}
+
+      </div>
+
+    </div>
+  );
+}

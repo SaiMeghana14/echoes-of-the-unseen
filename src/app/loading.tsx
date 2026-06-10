ccc2244515f05237bbcf456eb94bd0ae51cedbcc
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-06-10 15:30:11 +0530

    Create loading.tsx

---
+++ src/app/loading.tsx
@@ -0,0 +1,15 @@
+export default function Loading() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div>
+        <h1 className="text-4xl font-bold">
+          🌌 Echoes of the Unseen
+        </h1>
+
+        <p className="mt-4 text-gray-400">
+          Scanning Humanity's Memory...
+        </p>
+      </div>
+    </div>
+  );
+}

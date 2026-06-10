f71c87055fb58e5cc8db1c5466a143f38d68e834
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-06-10 11:00:17 +0530

    Create ThreatMap.tsx

---
+++ src/components/dashboard/ThreatMap.tsx
@@ -0,0 +1,31 @@
+export default function ThreatMap() {
+  return (
+    <div
+      className="
+      glass
+      rounded-3xl
+      p-8
+      min-h-[400px]
+    "
+    >
+      <h2 className="text-3xl font-bold mb-6">
+        🌍 Threat Map
+      </h2>
+
+      <div
+        className="
+        h-[300px]
+        rounded-3xl
+        border
+        border-white/10
+        flex
+        items-center
+        justify-center
+      "
+      >
+        Connect VanishingEarth Globe Here
+      </div>
+
+    </div>
+  );
+}

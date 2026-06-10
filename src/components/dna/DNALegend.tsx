cc8142e336ef2ecdda8f611b8491055acbee268c
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-06-10 10:45:28 +0530

    Create DNALegend.tsx

---
+++ src/components/dna/DNALegend.tsx
@@ -0,0 +1,35 @@
+export default function DNALegend() {
+  return (
+    <div
+      className="
+      mt-8
+      p-6
+      rounded-3xl
+      glass
+      flex
+      flex-wrap
+      gap-6
+    "
+    >
+      <div>
+        🟡 Culture Core
+      </div>
+
+      <div>
+        🟣 Knowledge
+      </div>
+
+      <div>
+        🔵 Stories
+      </div>
+
+      <div>
+        🟢 Rituals
+      </div>
+
+      <div>
+        💠 Beliefs
+      </div>
+    </div>
+  );
+}

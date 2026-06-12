206acff8a29e0b9ac4c2404acf1c819db1c1d387
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-06-12 16:22:27 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,12 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
 
-import Navbar from "@/components/common/Navbar";
 import FloatingNav from "@/components/common/FloatingNav";
 import Footer from "@/components/common/Footer";
 
 export const metadata: Metadata = {
   title: "Echoes of the Unseen",
-  description:
-    "AI Guardian of Human Memory",
+  description: "AI Guardian of Human Memory",
 };
 
 export default function RootLayout({
@@ -20,9 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <FloatingNav />
-
         {children}
-
         <Footer />
       </body>
     </html>

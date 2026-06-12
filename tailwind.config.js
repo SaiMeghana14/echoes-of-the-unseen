46c86c332dfa34d39f05848b59c3fe26d3109fec
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-06-12 16:11:00 +0530

    Update and rename tailwind.config.ts to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-
   theme: {
     extend: {
       colors: {
@@ -15,8 +13,5 @@ const config: Config = {
       },
     },
   },
-
   plugins: [],
 };
-
-export default config;

dbbe21dee086dc1e1e827c57bb6190485ef530be
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-06-12 16:05:09 +0530

    Update FloatingNav.tsx

--- src/components/common/FloatingNav.tsx
+++ src/components/common/FloatingNav.tsx
@@ -27,24 +27,7 @@ const links = [
 
 export default function FloatingNav() {
   return (
-    <nav
-      className="
-      fixed
-      top-6
-      left-1/2
-      -translate-x-1/2
-      z-[999]
-      backdrop-blur-xl
-      bg-white/10
-      border
-      border-white/20
-      rounded-full
-      px-6
-      py-3
-      flex
-      gap-6
-      shadow-2xl
-    "
+    <nav className="bg-red-500 p-10">
     >
       {links.map((link) => (
         <Link

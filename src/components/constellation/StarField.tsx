fdbac9472ae56882db4e6dd37df9e5774cab1bf5
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-06-12 12:06:53 +0530

    Update StarField.tsx

--- src/components/constellation/StarField.tsx
+++ src/components/constellation/StarField.tsx
@@ -1,11 +1,36 @@
-<Particles
- options={{
-   particles:{
-     number:{value:80},
-     move:{speed:0.5},
-     links:{
-       enable:true
-     }
-   }
- }}
-/>
+"use client";
+
+import Particles from "react-tsparticles";
+
+export default function StarField() {
+  return (
+    <Particles
+      id="tsparticles"
+      className="fixed inset-0 -z-10"
+      options={{
+        particles: {
+          number: {
+            value: 80,
+          },
+
+          links: {
+            enable: true,
+          },
+
+          move: {
+            enable: true,
+            speed: 1,
+          },
+
+          opacity: {
+            value: 0.4,
+          },
+
+          size: {
+            value: 2,
+          },
+        },
+      }}
+    />
+  );
+}

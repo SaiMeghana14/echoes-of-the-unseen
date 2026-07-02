3cbdd234b3e3c00c946115bc8f2fc9a29e0300dc
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-07-02 20:24:18 +0530

    Update EndangeredFeed.tsx

--- src/components/home/EndangeredFeed.tsx
+++ src/components/home/EndangeredFeed.tsx
@@ -19,18 +19,25 @@ const endangeredItems = [
 export default function EndangeredFeed() {
   return (
     <section className="py-24 px-6">
+      
+      <div className="text-xs text-gray-500">
+      #{index+1}
+      </div>
 
       <h2 className="text-5xl font-bold mb-12">
         🚨 Endangered Today
       </h2>
 
-      <div className="grid md:grid-cols-3 gap-8">
+      <div className="grid
+           grid-cols-2
+           lg:grid-cols-6
+           gap-5">
 
-        {endangeredItems.map((item) => (
+        {endangeredItems.map((item,index)=>(
           <div
             key={item.name}
             className="
-            p-8
+            p-5
             rounded-3xl
             border
             border-red-500/20
@@ -45,13 +52,36 @@ export default function EndangeredFeed() {
               {item.country}
             </p>
 
-            <div className="mt-6 text-5xl text-red-400 font-bold">
+            <div className="mt-6 text-4xl text-red-400 font-bold">
               {item.risk}%
             </div>
+            <div className="mt-4 h-2 rounded-full bg-white/10">
 
-            <p className="mt-2">
-              Extinction Risk
-            </p>
+            <div
+            className="h-full rounded-full bg-red-500"
+            style={{
+            width:`${item.risk}%`
+            }}
+            />
+            
+            </div>
+
+            <div className="mt-4">
+
+            <span className="
+            px-2
+            py-1
+            rounded-full
+            text-xs
+            bg-red-500/20
+            text-red-300
+            ">
+            
+            🔴 Critical
+            
+            </span>
+            
+            </div>
           </div>
         ))}
 
